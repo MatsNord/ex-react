f120e0f788699c56ff345ae33d5b642b98fa2971
Mats Nord <mnd@qlik.com>
2017-12-20 01:01:48 +0100

    Code scouting and added deps to module

--- src/App.js
+++ src/App.js
@@ -4,36 +4,29 @@ import "./App.css";
 import ModuleB from "./extensions/PluginB";
 import LazyA from "./extensions/PluginA";
 
-function CreateApp() {
-  class App extends Component {
-    render() {
-      const B = ModuleB.dependencies({});
-      // console.log("C", this.AA);
-      //const A = App.AA.default.ependencies({});
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            <em>Content</em>
-          </p>
-          <B name="Plugin B" description="A pluggable extension named B" />
-          {
-            //<A name="Plugin A" description="A lazy pluggable extension named A" />
-          }
-          <LazyA
-            name="Plugin Lazy A"
-            description="A lazy loaded pluggable extension named A"
-          />
-        </div>
-      );
-    }
+class App extends Component {
+  render() {
+    const B = ModuleB.dependencies({ routing: "routingDep" });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <em>Content</em>
+        </p>
+        <B name="Plugin B" description="A pluggable extension named B" />
+        {
+          //<A name="Plugin A" description="A lazy pluggable extension named A" />
+        }
+        <LazyA
+          name="Plugin Lazy A"
+          description="A lazy loaded pluggable extension named A"
+        />
+      </div>
+    );
   }
-  return App;
 }
-
-const App = CreateApp();
 
 export default App;
--- src/extensions/PluginB/index.js
+++ src/extensions/PluginB/index.js
@@ -3,10 +3,12 @@ import React from "react";
 const dependencies = options => {
   class B extends React.Component {
     render() {
+      const opt = options;
       return (
         <div className="App-comp">
           <h1>{this.props.name}</h1>
           <div className="description">{this.props.description}</div>
+          <div>Dependencies: {opt.routing}</div>
         </div>
       );
     }
